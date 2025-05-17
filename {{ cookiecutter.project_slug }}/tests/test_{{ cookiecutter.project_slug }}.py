6fbb790b93569bfb26a6230d90fba544733dd84e
Chaam Zeina <zeinachaam@gmail.com>
2025-05-17 16:24:17 +0100

    added sample test and coveragerc, removed ci for template - format trips up checks

---
+++ {{ cookiecutter.project_slug }}/tests/test_{{ cookiecutter.project_slug }}.py
@@ -0,0 +1,12 @@
+import unittest
+
+from {{cookiecutter.project_slug}} import {{ cookiecutter.project_slug }}
+
+
+class Test{{ cookiecutter.project_slug|title }}(unittest.TestCase):
+
+    def test_{{ cookiecutter.project_slug }}(self):
+        """Given: """
+        """When: """
+        """Then: """
+        self.assertTrue(1)
